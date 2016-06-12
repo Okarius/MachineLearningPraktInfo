1687bb882b15d4a8d39980107969cdbf6cec813b
alex <alexanderdiegel@gmx.de>
2016-06-12 20:43:14 +0200

    kleine korrektur Winter hat index 1 statt 4

--- Blatt_6/BayesNet.m
+++ Blatt_6/BayesNet.m
@@ -2,7 +2,7 @@ function [ P ] = BayesNet( A, B, C, D, X )
 %BayesNet Berechnet Wahrscheinlichkeit im Netz
 %   Zustaende als Zahlen
 %%
-%A:= (0 -Keine Angabe, 1 -Fruehling, 2 -Sommer, 3 -Herbst, 4 -Winter)
+%A:= (0 -Keine Angabe, 1 -Winter, 2 -Fruehling, 3 -Sommer, 4 -Herbst)
 %B:= (0 -Keine Angabe, 1 -Nordatlantik, 2 -Suedatlantik)
 %C:= (0 -Keine Angabe, 1 -hell, 2 -mittel, 3 -dunkel)
 %D:= (0 -Keine Angabe, 1 -breit, 2 -duenn)
@@ -29,10 +29,10 @@ TBL_DX=[
     0.3,0.7;
     0.6,0.4;
     ];
-%%
+
 %Check inputs
 % hier muss evtl noch weiter Fehler abgefangen werden je nach Kombination.
-A = 2;B = 1;C = 0;D = 2;X = 2;
+%A = 3;B = 1;C = 0;D = 2;X = 2;
 % Mein Test liefert 
 if A < 1
     A=1:4;
@@ -49,18 +49,16 @@ if D < 1
 end
 if X < 1
     X =1:2;
-else
-%%
+end
 % P(A,B,C,D,X) = P(A)*P(B)*P(X|A,B)*P(C|X)*P(D|X)
-
 %Machs mal mit for schleifen...
-sum=0
+sum=0;
 for a=A
     for b= B
         for c=C
             for d=D
                 for x=X
-                    sum = sum + TBL_PA(a)*TBL_PB(b)*TBL_XAB(2*(a-1)+b,x)*TBL_CX(x,c)*TBL_DX(x,d)
+                    sum = sum + TBL_PA(a)*TBL_PB(b)*TBL_XAB(2*(a-1)+b,x)*TBL_CX(x,c)*TBL_DX(x,d);
                 end
             end
         end
@@ -69,6 +67,6 @@ end
 
 %val = sum(TBL_PA(A)*TBL_PB(B)*TBL_XAB(indices ,X)* TBL_CX(X,C)*TBL_DX(X,D))
 
-P=sum
+P=sum;
 end
 
