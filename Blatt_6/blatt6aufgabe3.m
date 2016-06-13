2d1f104c0afe1e068df372c5cec31a3ba28247ce
alex <alexanderdiegel@gmx.de>
2016-06-13 20:07:16 +0200

    Aufgabe 2 getecht Bitte mal noch einer Nachrechnen

--- Blatt_6/blatt6aufgabe3.m
+++ Blatt_6/blatt6aufgabe3.m
@@ -2,12 +2,11 @@
 % Aufgabe 3
 % 
 %%
-pxc = BayesNet(3,0,0,1,2)
-pc = BayesNet(0,0,0,1,2)
+p1 = BayesNet(3,1,0,2,2)
+p2 = BayesNet(1,1,1,2,1)
 
-aa=pxc/pc
+c = BayesNet(0,0,2,0,1)/BayesNet(0,0,2,0,0)
 
-xxx1 = BayesNet(0,0,1,2,1)
-xxx2 = BayesNet(0,0,0,2,1)
+d = BayesNet(3,0,0,1,2)/BayesNet(0,0,0,1,2)
 
-xxx3= xxx1/xxx2
+e = BayesNet(0,0,1,2,1)/BayesNet(0,0,0,2,1)
